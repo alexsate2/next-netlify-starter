fa785ee233bc19ff056c7e790b7962c6ce21d551
Alex Teixeira <alex.teixeira@orium.com>
2024-04-03 11:05:42 -0300

    Add a fetch external call

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,3 +1,23 @@
-export default () => new Response("Hello");
+//export default () => new Response("Hello");
+
+
+// Let's serve an image of a kitten from the internet
+
+export default async () => {
+  // fetch() is supported natively by Deno!
+  // Returning the awaited response automatically sets the
+  // content-type headers!
+
+
+  const r = await fetch("https://api.vercel.com/v4/aliases/example.vercel.app?from=1540095775951&projectId=prj_12HKQaOmR5t5Uy6vdcQsNIiZgHGB&since=1540095775941&teamId=SOME_STRING_VALUE&until=1540095775951", {
+    "headers": {
+      "Authorization": "Bearer <TOKEN>"
+    },
+    "method": "get"
+  })
+    return Response.json(await r.json());
+
+
+};
 
 export const config = { path: "/test" };
